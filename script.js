cbda001998beeeb17f59ba6c2216be1f55809482
s5ii <sultanalmarri156@gmail.com>
2026-02-03 18:32:29 +0400

    Update script.js

--- script.js
+++ script.js
@@ -23,7 +23,7 @@ let lowGravityZone = false;
 // ===== فيزياء ناعمة وبطيئة =====
 let gravityUp = 0.7;
 let gravityDown = 0.9;
-const jumpPower = 7;
+const jumpPower = 4;
 const moveSpeed = 2;
 
 // إنشاء اللاعب
@@ -274,5 +274,6 @@ function restartGame() {
 createPlayer();
 createPlatforms();
 gameLoop();
+
 
 
